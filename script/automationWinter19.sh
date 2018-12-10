e9941e7fe9c705f6a2019fd63a505a77a63f1727
Marcus Hammarberg <marcusoft.net@gmail.com>
2018-12-10 14:05:45 +0100

    Testing again

--- script/automationWinter19.sh
+++ script/automationWinter19.sh
@@ -1,7 +1,6 @@
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-git clone https://github.com/saltsthlm/salt-jsfs-dotfiles.git ~/.dotfiles
-bash ~/.dotfiles/script/bootstrap
-
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed 's:env zsh -l::g' | sed 's:chsh -s .*$::g')"
 
+git clone https://github.com/saltsthlm/salt-jsfs-dotfiles.git ~/.dotfiles
+bash ~/.dotfiles/script/bootstrap
