816e0b9f06847ead0e2413c015ca0661fbf9c621
Marcus Hammarberg <marcusoft.net@gmail.com>
2018-12-10 09:47:46 +0100

    Ugly hack to not go into zsh after install

--- script/automationWinter19.sh
+++ script/automationWinter19.sh
@@ -2,6 +2,6 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 
 git clone https://github.com/saltsthlm/salt-jsfs-dotfiles.git ~/.dotfiles
 
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed 's:env zsh -l::g' | sed 's:chsh -s .*$::g')"
 
 bash ~/.dotfiles/script/bootstrap
