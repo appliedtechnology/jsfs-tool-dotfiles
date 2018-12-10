d2bd75c317ca7bbb7175e791227be0a76610d9d2
Marcus Hammarberg <marcusoft.net@gmail.com>
2018-12-10 12:51:57 +0100

    Moved the oh my zsh install

--- script/automationWinter19.sh
+++ script/automationWinter19.sh
@@ -1,9 +1,7 @@
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 git clone https://github.com/saltsthlm/salt-jsfs-dotfiles.git ~/.dotfiles
-
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed 's:env zsh -l::g' | sed 's:chsh -s .*$::g')"
-
 bash ~/.dotfiles/script/bootstrap
 
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed 's:env zsh -l::g' | sed 's:chsh -s .*$::g')"
 
