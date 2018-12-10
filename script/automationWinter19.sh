b0d50c338d750a7a6c513037292f4575d0af07cc
Marcus Hammarberg <marcusoft.net@gmail.com>
2018-12-10 11:07:43 +0100

    Updated some more things in the script and added another heroku

--- script/automationWinter19.sh
+++ script/automationWinter19.sh
@@ -5,3 +5,6 @@ git clone https://github.com/saltsthlm/salt-jsfs-dotfiles.git ~/.dotfiles
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed 's:env zsh -l::g' | sed 's:chsh -s .*$::g')"
 
 bash ~/.dotfiles/script/bootstrap
+
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
