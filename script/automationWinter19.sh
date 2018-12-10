2c29faefd18b6ede469a129d87c5b67e9921f03e
Marcus Hammarberg <marcusoft.net@gmail.com>
2018-12-10 15:31:37 +0100

    Tried with ohmyzsh at the top

--- script/automationWinter19.sh
+++ script/automationWinter19.sh
@@ -1,6 +1,6 @@
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed 's:env zsh -l::g' | sed 's:chsh -s .*$::g')"
 
-# sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed 's:env zsh -l::g' | sed 's:chsh -s .*$::g')"
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 git clone https://github.com/saltsthlm/salt-jsfs-dotfiles.git ~/.dotfiles
 bash ~/.dotfiles/script/bootstrap
