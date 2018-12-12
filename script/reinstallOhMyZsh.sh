414935ff1d6ed6a2af511ddc6e6d79df66d03574
Marcus Hammarberg <marcusoft.net@gmail.com>
2018-12-12 11:35:22 +0100

    Update reinstallOhMyZsh.sh

--- script/reinstallOhMyZsh.sh
+++ script/reinstallOhMyZsh.sh
@@ -1,4 +1,4 @@
-#! 
+#!/usr/bin/env bash
 bash ~/.oh-my-zsh/tools/uninstall.sh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 bash ~/.dotfiles/script/bootstrap
