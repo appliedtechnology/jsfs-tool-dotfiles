56a5ae527d610c611d271a0b7e47b3f4a19cb43a
Marcus Hammarberg <marcusoft.net@gmail.com>
2019-01-10 11:40:18 +0100

    Moved the chown command to top

--- script/updateNvmWinter19.sh
+++ script/updateNvmWinter19.sh
@@ -1,5 +1,8 @@
 #!/bin/zsh
 
+echo ">>>>>Updating ownership on config"
+sudo chown -R $USER:$(id -gn $USER) ~/.config
+
 echo ">>>>>Pulling latests dotfiles"
 cd ~/.dotfiles
 git pull
@@ -7,9 +10,6 @@ git pull
 echo ">>>>>Uninstallning node from homebrew"
 brew uninstall --ignore-dependencies --force node
 brew uninstall --ignore-dependencies --force node@10
-
-echo ">>>>>Updating ownership on config"
-sudo chown -R $USER:$(id -gn $USER) ~/.config
 
 echo ">>>>>Running dot command"
 dot
