01dfb20f55145343e45fb4dd713e0c6ef74d13b7
Marcus Hammarberg <marcusoft.net@gmail.com>
2019-01-10 11:27:55 +0100

    Added echo statements

--- script/updateNvmWinter19.sh
+++ script/updateNvmWinter19.sh
@@ -1,18 +1,22 @@
 #!/bin/zsh
 
-sudo chown -R $USER:$(id -gn $USER) ~/.config
-
+echo "Pulling latests dotfiles"
 cd ~/.dotfiles
 git pull
 
+echo "Uninstallning node from homebrew"
 brew uninstall --ignore-dependencies --force node
 brew uninstall --ignore-dependencies --force node@10
 
+echo "Updating ownership on config"
+sudo chown -R $USER:$(id -gn $USER) ~/.config
+
+echo "Running dot command"
 dot
 
+echo "Setting correct version of node"
 . $(brew --prefix nvm)/nvm.sh
 nvm install v10.15.0
 nvm alias default v10.15.0
 nvm use default
-
 node -v
