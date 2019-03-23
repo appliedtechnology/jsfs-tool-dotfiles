b9aff950792f6c1cfdaf06bf15516b4a74cd3881
Marcus Hammarberg <marcusoft.net@gmail.com>
2019-03-23 12:11:59 +0100

    Updated the script for class of Spring 19

--- script/automationSpring19.sh
+++ script/automationSpring19.sh
@@ -1,3 +1,12 @@
+echo ">>>>>Installing homebrew"
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+echo ">>>>>Bootstrapping all the applications for the first time"
 git clone https://github.com/saltsthlm/salt-jsfs-dotfiles.git ~/.dotfiles
 bash ~/.dotfiles/script/bootstrap
+
+echo ">>>>>Setting correct version of node for the course"
+. $(brew --prefix nvm)/nvm.sh
+nvm install v10.15.0
+nvm alias default v10.15.0
+nvm use default
