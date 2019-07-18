5c63b4a0ac3805c19065af2f2ffc750c7f3f2cb0
Marcus Hammarberg <marcusoft.net@gmail.com>
2019-07-18 09:53:34 +0200

    Added Automation Script for Fall19

---
+++ script/automationFall19.sh
@@ -0,0 +1,16 @@
+echo ">>>>Updating OSX stuff"
+sudo softwareupdate -i -a
+
+echo ">>>>>Installing homebrew"
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+echo ">>>>>Bootstrapping all the applications for the first time"
+git clone https://github.com/saltsthlm/salt-jsfs-dotfiles.git ~/.dotfiles
+bash ~/.dotfiles/script/bootstrap
+
+
+echo ">>>>>Setting correct version of node for the course"
+. ~/.dotfiles/nvm/nvm.zsh
+nvm install v10.15
+nvm alias default v10.15
+nvm use default
